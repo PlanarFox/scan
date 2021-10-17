3e2e39d5c66cfbf0e3c6f0885dd8e1ad1f025d5f
planarfox <planarfox@planarfox.online>
2021-10-17 15:49:11 +0000

    total num bug fix

--- server/submit_proc.py
+++ server/submit_proc.py
@@ -14,6 +14,12 @@ def zmap(cwd, uuid, probe, **kw):
                 while True:
                     result_line = result.readline()
                     if not result_line:
+                        with open(os.path.join(probe_cwd, 'target.txt'), 'r') as target:
+                            while True:
+                                line = target.readline()
+                                if not line:
+                                    break
+                                total_num += 1 
                         break
                     with open(os.path.join(probe_cwd, 'target.txt'), 'r') as target:
                         while True:
